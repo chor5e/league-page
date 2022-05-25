b0928a8452ebdfb0ba73d79ae22d292365daddff
chor5e <106204969+chor5e@users.noreply.github.com>
2022-05-25 18:17:14 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -117,7 +117,7 @@ export const managers = [
       "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Nate",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "I didn't come here to play school", // (optional)
+      "location": "I didn't come here to play school.", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/nate.JPG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football

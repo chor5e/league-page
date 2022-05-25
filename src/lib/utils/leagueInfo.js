ed93832fde0e88ffbcb1092809087ee30a1d66b3
chor5e <106204969+chor5e@users.noreply.github.com>
2022-05-25 14:38:45 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,7 +27,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
      {
        "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "Caleb AKA Horse",
+       "name": "Caleb aka Horse",
        "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "THE Ohio State University", // (optional)
        "bio": "I am a second generation fantasy football addict who consistently over-extends my faculties during the football season. I manage between 5-9 fantasy rosters in any given season, although my entry into this league certainly receives the lion's share of my time and attention. While I tend to form deep (almost personal) relationships with each of my Dynasty players, I have been known to offer many of them up in questionable trades after a few weeknight whiskeys. As one of the founding owners, I am proud to see how far the league has come -- after a quiet inaugural season, it is now commonplace to see a blockbuster trade hit at 9:30 pm on a random Wednesday in March. I get to compete and interact with some of my best pals and a brother-in-law for 365 days a year...and as a 30 year old father living in an apartment with all females, this league is an oasis for me. ",
@@ -49,7 +49,7 @@ export const managers = [
      },
     {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "Brandon AKA Big Daddy Dinosaur Eggs",
+      "name": "Brandon aka Big Daddy Dinosaur Eggs",
       "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "The Sack", // (optional)
       "bio": "Lorem ipsum...",
@@ -97,7 +97,7 @@ export const managers = [
       "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "West Texas Community College University", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/cody.JPG", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/cody2.JPG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)

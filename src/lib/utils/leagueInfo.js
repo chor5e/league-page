53334a9aa522273c2215bb8139fb70d638e49b57
chor5e <106204969+chor5e@users.noreply.github.com>
2022-09-15 12:09:42 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,7 +27,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
      {
        "roster": 7,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "Caleb aka Horse",
+       "name": "Caleb",
        "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "THE Ohio State University", // (optional)
        "bio": "I am a second generation fantasy football addict who consistently over-extends my faculties during the football season. I manage between 5-9 fantasy rosters in any given season, although my entry into this league certainly receives the lion's share of my time and attention. While I tend to form deep (almost personal) relationships with each of my Dynasty players, I have been known to offer many of them up in questionable trades after a few weeknight whiskeys. As one of the founding owners, I am proud to see how far the league has come -- after a quiet inaugural season, it is now commonplace to see a blockbuster trade hit at 9:30 pm on a random Wednesday in March. I get to compete and interact with some of my best pals and a brother-in-law for 365 days a year...and as a 30 year old father living in an apartment with all females, this league is an oasis for me. ",
@@ -93,7 +93,7 @@ export const managers = [
     },
    {
       "roster": 4,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "Cody aka Yodith",
+      "name": "Cody aka Yodi",
       "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "West Texas Community College University", // (optional)
       "bio": "Lorem ipsum...",
@@ -227,24 +227,24 @@ export const managers = [
     },
    {
       "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "Zack",
-      "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      "name": "Zak",
+      "tookOver": 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Ohio", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/forker.JPG", // square ratio recommended (no larger than 500x500)
+      "photo": null, // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": cin, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Heath", // Can be anything (usually your rival's name)
-        link: 6, // manager array number within this array, or null to link back to all managers page
+        name: "Big-bro-in-law", // Can be anything (usually your rival's name)
+        link: 1, // manager array number within this array, or null to link back to all managers page
         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "I haven't filled out my form yet",
-      "tradingScale": 8, // 1 - 10
+      "tradingScale": 9, // 1 - 10
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
   
